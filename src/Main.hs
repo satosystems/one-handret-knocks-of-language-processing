fcbc7f9e174c76c416281e18c9d69bbfbb585731
Satoshi Ogata <satosystems@gmail.com>
2018-01-02 04:10:50 +0900

    implement question 04

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ main = do
   a01
   a02
   a03
+  a04
 
 -- 00. 文字列の逆順
 -- 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
@@ -39,4 +40,16 @@ a03 :: IO ()
 a03 = do
   print $ map (length . filter isAlpha) $ words "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
   printSeparator
+
+-- 04. 元素記号
+-- "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
+a04 :: IO ()
+a04 = do
+  print $ fn [1, 5, 6, 7, 8, 9, 15, 16, 19] 1 (words "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.") []
+  printSeparator
+ where
+  fn _ _ [] rs = reverse rs
+  fn ns n (w:ws) rs = fn ns (n + 1) ws ((n, (take len w)):rs)
+   where
+    len = if n `elem` ns then 1 else 2
 
