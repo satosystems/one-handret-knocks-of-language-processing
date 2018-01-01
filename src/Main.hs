48f3085f0c2e438d87fda04c411b1dbbaf2392c1
Satoshi Ogata <satosystems@gmail.com>
2018-01-01 17:44:48 +0900

    implement question 00

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,14 @@
+printSeparator :: IO ()
+printSeparator = putStrLn "--------------------"
+
 main :: IO ()
-main = undefined
+main = do
+  a00
+
+-- 00. 文字列の逆順
+-- 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+a00 :: IO ()
+a00 = do
+  putStrLn $ reverse "stressed"
+  printSeparator
 
